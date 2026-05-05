a6d2c6534c39e0a9af59095fef1d5df74d7ae911
Bilal Mohammed <bilalmd@umich.edu>
2026-05-05 12:43:26 -0400

    Delete Advection Solver directory

--- Advection Solver/enforce_bc_left_coeff_RK4.m
+++
@@ -1,8 +0,0 @@
-function a = enforce_bc_left_coeff_RK4(a)
-    N = length(a)-1;
-    s = 0;
-    for n = 0:N-1
-        s = s + ((-1)^n) * a(n+1);
-    end
-    a(N+1) = ((-1)^(N+1)) * s; %last a term
-end
--- Advection Solver/rhs_coeff_RK4.m
+++
@@ -1,5 +0,0 @@
-function dadt = rhs_coeff_RK4(a, c)
-    N = length(a)-1;
-    b = b_Chebyshev_coeff_RK4(a, N);
-    dadt = -c * b;
-end
